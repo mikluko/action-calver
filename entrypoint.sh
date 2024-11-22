36c5a788e2e0a62c3db9d6adfca1630210f425db
Mikhail Lukianchenko <42915+mikluko@users.noreply.github.com>
2024-11-22 14:59:05 +0100

    fix: update argument parsing and handle calver fallback

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,24 @@ eval set -- "$PARSED_OPTIONS"
 
 while true; do
   case "$1" in
-    -l|--layout) layout="$2"; shift 2 ;;
-    --) shift; break ;;
-    *) echo "Usage: cmd [-l layout] [--layout layout]"; exit 1 ;;
+  -l | --layout)
+    layout="$2"
+    shift 2
+    ;;
+  --)
+    shift
+    break
+    ;;
+  *)
+    echo "Usage: cmd [-l layout] [--layout layout]"
+    exit 1
+    ;;
   esac
 done
 
 git config --global --add safe.directory /github/workspace
 
-prev_version=$(gh release list --json tagName | jq -r '.[].tagName' | calver --layout "${layout}")
+prev_version=$(gh release list --json tagName | jq -r '.[].tagName' | calver --layout "${layout}" || calver --layout "${layout}")
 next_version=$(echo "$prev_version" | calver --layout "${layout}" --next)
 
 cat <<EOF | tee "${GITHUB_OUTPUT:-/dev/null}"
