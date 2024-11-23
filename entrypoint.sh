a9752ea49fc81b736167a351664c6c086a3d7467
Mikhail Lukianchenko <42915+mikluko@users.noreply.github.com>
2024-11-23 17:21:38 +0100

    chore: rework release process (#1)
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,41 @@
 set -euo pipefail
 
 layout=vYY.0M.MICRO-MODIFIER
-modifier="${1:-}"
+modifier=
 
-if [[ -n "${GITHUB_ACTIONS:-}" ]]; then git config --global --add safe.directory /github/workspace; fi
+case "$GITHUB_EVENT_NAME" in
+push)
+  case "$GITHUB_REF" in
+  refs/heads/main | refs/heads/master)
+    series=stable
+    ;;
+  refs/heads/develop)
+    series=beta
+    modifier=${series}.${GITHUB_RUN_NUMBER}.${GITHUB_SHA:0:7}
+    ;;
+  *)
+    series=alpha
+    modifier=${series}.${GITHUB_RUN_NUMBER}.${GITHUB_SHA:0:7}
+    ;;
+  esac
+  ;;
+pull_request)
+  series="pr$(jq '.pull_request.number' "$GITHUB_EVENT_PATH")"
+  modifier=${series}.${GITHUB_RUN_NUMBER}.${GITHUB_SHA:0:7}
+  ;;
+*)
+  cat <<EOF >&2
+::error::Unsupported event name: '$GITHUB_EVENT_NAME'
+EOF
+  exit 1
+  ;;
+esac
 
-version=$(gh release list --json tagName | jq -r '.[].tagName' | calver --trim-suffix --layout=$layout --next --modifier=$modifier 2>/dev/null || true)
+version=$(gh release list --json tagName | jq -r '.[].tagName' | calver --trim-suffix --layout=$layout --next --modifier="$modifier" 2>/dev/null || true)
 
-[[ -z "${version}" ]] && version=$(calver --layout=$layout | calver --next --layout=$layout --modifier=$modifier --trim-suffix)
+[[ -z "${version}" ]] && version=$(calver --layout=$layout | calver --next --layout=$layout --modifier="$modifier" --trim-suffix)
 
 cat <<EOF | tee "${GITHUB_OUTPUT:-/dev/null}"
+series=${series}
 version=${version}
 EOF
--- templates/README.md
+++ templates/README.md
@@ -18,7 +18,7 @@ jobs:
     steps:
       - name: Generate Calendar Version
         id: calver
-        uses: mikluko/action-calver@v24.11.7-pr1.39
+        uses: mikluko/action-calver@${VERSION}
         with:
           token: ${{ github.token }}
 
